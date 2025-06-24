c1acb8e9382ef617f3b719b70a4263904f06f9bf
nazmgb <nazmgb@gmail.com>
2025-06-24 12:06:33 -0600

    Redireccion del home

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Home from "./views/home.jsx";
 import Host from "./views/host.jsx";
 import Viewer from "./views/viewer.jsx";
 import Card from "./views/card.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+     <Route path="/home" element={<Home />} />
       <Route path="/host" element={<Host />} />
       <Route path="/viewer" element={<Viewer />} />
       <Route path="/card" element={<Card />} />
