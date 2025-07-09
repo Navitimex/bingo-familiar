f351773fd826752efc00b400baf097ced8ca1803
kevartav <kevinartavia29@gmail.com>
2025-07-09 01:24:38 -0600

    Funciona localmente pero no en web

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+const basename = import.meta.env.PROD ? "/bingo-familiar/" : "/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/bingo-familiar/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
 );
