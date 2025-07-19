447cafda822fbfc51d6e75734c9a8318bdb4a956
Nazaret Gonzalez <nazmgb@gmail.com>
2025-07-19 04:50:38 -0600

    Delete de import innecesarios

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Home from "./views/home.jsx";
 import Host from "./views/host.jsx";
 import Viewer from "./views/viewer.jsx";
-import Card from "./views/card.jsx";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
@@ -14,7 +13,6 @@ function App() {
      <Route path="/home" element={<Home />} />
       <Route path="/host" element={<Host />} />
       <Route path="/viewer" element={<Viewer />} />
-      <Route path="/card" element={<Card />} />
     </Routes>
   )
 }
